aba0ed7494cba67160b7904e6d225cbe176f10f6
rbrock44 <rbrock44@users.noreply.github.com>
2026-08-17 03:38:14 +0000

    deploy: 44f978e22587b3da3dade9ee1bd1896fbf970b6f

--- chunk-C8cxIxbJ.js
+++ chunk-C8cxIxbJ.js
@@ -1,4 +1,4 @@
-import{E as Xu,F as fC,I as g,J as um,P as em,a as AT,c as FE,d as I_,f as Ju,j as _I,y as Qg}from"./main-3KNA2PWD.js";import{t as g$1}from"./chunk-C4R55sfY.js";var e={title:`About`,description:`Ryan Brock builds small, useful web applications and writes about how they were put together.`,html:`<p>I build small, useful web applications and then write about how they were put together.</p>
+import{E as Xu,F as fC,I as g,J as um,P as em,a as AT,c as FE,d as I_,f as Ju,j as _I,y as Qg}from"./main-TLOAEN55.js";import{t as g$1}from"./chunk-CoV55kyM.js";var e={title:`About`,description:`Ryan Brock builds small, useful web applications and writes about how they were put together.`,html:`<p>I build small, useful web applications and then write about how they were put together.</p>
 <p>Most of what I make ends up on a subdomain of <code>ryan-brock.com</code> \u2014 recipe collections,
 puzzle implementations, scorekeeping tools, a woodworking gallery, a handful of utilities.
 Almost all of it is Angular on the front end, deployed to GitHub Pages, with a Kotlin and
