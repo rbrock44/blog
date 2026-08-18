222860b594ec078696cadc2c828e57cdc94c7e2f
rbrock44 <rbrock44@users.noreply.github.com>
2026-08-18 04:08:38 +0000

    deploy: f30b88b565f05a91ae8c68a9c9e844bc7ec40563

---
+++ chunk-DfKB9jwa.js
@@ -0,0 +1,14 @@
+var e=`<p>It started because I had a cousin pass away, and so his parents, my aunt and uncle,
+decided to do organ donation, saving/assiting many lifes in the process. They got 
+involved with Saint Louis Transplant of America, and during the yearly trivia game 
+I inquired about how to change the organizers from using Excel. I found the format 
+to be a very poor user experience from the audience&#39;s perspective.</p>
+<p>The lady in charge said it would have to be some free tool, and at the time I was working
+a job that had 10% learning time, developing software for them. The tech stack there used Angular, 
+so I decided to practice my skills and make <a href="https://scorekeeping-by-rounds.ryan-brock.com/">a website</a>.
+I completed this <a href="https://github.com/rbrock44/scorekeeping-by-rounds">project</a>, and adoption turned out harder than I thought.</p>
+<p>To fundraise for his scholarship, my aunt and uncle held a disc golf tournament in his
+honor. They did that for several years, then migrated to a local trivia night they host
+every year. My niece has used this as scorekeeper every year, providing a great experience. 
+She can edit scores on one monitor and have the scores display on the monitor projected to the audience.</p>
+`;export{e as default};
